ab9bda797290c4f21e92dc614f406b8f291a79e2
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-08-20 15:42:23 +0100

    sets up sinatra with shotgun

---
+++ lib/app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'shotgun'
+
+get '/' do
+  "hello!"
+end
+
+get '/secret' do
+  "this page is secret, you should leave"
+end
+
+get '/not_secret' do
+  "you can stay here as long as you like"
+end
